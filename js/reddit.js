3ba7e228adae4bcef98531804d1fdf13383bbd39
Khanh H <Kkhanhhoang@outlook.com>
2020-02-29 10:45:11 -0800

    added meme slideshow

---
+++ js/reddit.js
@@ -0,0 +1,67 @@
+fetch('https://www.reddit.com/r/memes/top.json?sort=top&t=day&limit=20')
+.then(res => res.json())
+.then(res => res.data.children)
+.then(res => res.map(post => ({
+  author: post.data.author,
+  link: post.data.url,
+  img: post.data.thumbnail,
+  title: post.data.title
+})))
+.then(res => res.map(render))
+.then(res => console.log(res))
+
+var slideIndex = 1;
+
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+  
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+  
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
+
+document.onkeydown = function(e) {
+    e = e || window.event;
+    if (e.keyCode == '37') {
+        plusSlides(-1) //left <- show Prev image
+    } else if (e.keyCode == '39') {
+        // right -> show next image
+        plusSlides(1)
+    }
+}
+
+const render = post => {
+    const node = document.createElement('div');
+    node.className = 'mySlides fade';
+    node.style = "display: none;"
+    node.innerHTML = `
+        <img src="${post.link}" style="width: 100%"/>
+        `;
+    document.getElementById('memeDiv').appendChild(node);
+    const dots = document.createElement('span');
+    dots.className = "dot";
+    document.getElementById('meme-dots').appendChild(dots);
+    showSlides(slideIndex);
+
+}
+    
+
+
