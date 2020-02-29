009f220e534dc54c3d7d1fb4e2cbf7d59b732355
Duncan Applegarth <duncana513@gmail.com>
2020-02-29 11:04:40 -0800

    update blockchain code a bit

--- js/blockchain.js
+++ js/blockchain.js
@@ -9,23 +9,15 @@ class Block{
         this.hash = this.calculateHash();
     }
 
-    calculateHash(){
-        return SHA256(this.index + this.prev_Hash + this.timestamp + JSON.stringify(this.data)).toString();
-    }
+    calculateHash(){ return SHA256(this.index + this.prev_Hash + this.timestamp + JSON.stringify(this.data)).toString(); }
 }
 
 class BlockChain{
-    constructor(){
-        this.chain = [this.createGenesisBlock()];
-    }
+    constructor() { this.chain = [this.createGenesisBlock()]; }
 
-    createGenesisBlock(){
-        return new Block(0, "01/01/01", "Genesis block", "0");
-    }
+    createGenesisBlock() { return new Block(0, "01/01/01", "Genesis block", "0"); }
 
-    getLatestBlock(){
-        return this.chain[this.chain.length - 1]
-    }
+    getLatestBlock() { return this.chain[this.chain.length - 1] }
 
     addBlock(newBlock){
         newBlock.prev_Hash = this.getLatestBlock().hash;
@@ -38,12 +30,8 @@ class BlockChain{
             const currentBlock = this.chain[i];
             const previousBlock = this.chain[i - 1];
 
-            if (currentBlock.hash !== currentBlock.calculateHash()) {
-                return false;
-            }
-            if (currentBlock.prev_Hash !== previousBlock.hash) {
-                return false;
-            }
+            if (currentBlock.hash !== currentBlock.calculateHash()) return false; 
+            if (currentBlock.prev_Hash !== previousBlock.hash) return false;
         }
         return true;
     }
@@ -57,5 +45,3 @@ Coin.addBlock(new Block(3, "12/2/12", { amount: 6}));
 
 console.log("Is blockchain valid? " + Coin.isChainValid());
 console.log(JSON.stringify(Coin, null, 4));
-
-
